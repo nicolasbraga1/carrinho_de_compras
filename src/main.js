2b4e4ace0501050fba84c6eba35fb237e2facce4
Nícolas <nicolasbrga01@gmail.com>
2022-11-16 17:53:32 -0300

    requisito 4 feito(crise evitada)

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@ import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { createProductElement } from './helpers/shopFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
-import product from './tests/mocks/product';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 window.onload = async () => {
+  const products = document.querySelector('.products');
+  products.innerHTML = '<p class = "loading">Carregando...</p>';
   const productsList = await fetchProductsList('computador');
+  products.innerHTML = '';
   productsList.forEach((product) => {
-    const products = document.querySelector('.products');
     products.appendChild(createProductElement(product));
-  })
-}
+  });
+};
