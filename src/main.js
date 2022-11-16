f8f4b34e16bc7481a3e81e84b91a5a5a78bda673
Nícolas <nicolasbrga01@gmail.com>
2022-11-16 18:36:15 -0300

    esse lint acaba comigo

--- src/main.js
+++ src/main.js
@@ -6,16 +6,17 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const products = document.querySelector('.products');
- window.onload = async () => {
+window.onload = async () => {
   products.innerHTML = '<p class = "loading">Carregando...</p>';
   try {
-   const productsList = await fetchProductsList('computador');
-   products.innerHTML = '';
-   productsList.forEach((product) => {
-   products.appendChild(createProductElement(product));
-  });
-} catch(error){
-   products.innerHTML = '';
-   products.innerHTML = '<p class = "error">Algum erro ocorreu, recarregue a página e tente novamente</p>"';
-}
+    const productsList = await fetchProductsList('computador');
+    products.innerHTML = '';
+    productsList.forEach((product) => {
+      products.appendChild(createProductElement(product));
+    });
+  } catch (error) {
+    products.innerHTML = '';
+    const message = 'Algum erro ocorreu, recarregue a página e tente novamente';
+    products.innerHTML = `<p class = "error">${message}</p>`;
+  }
 };
