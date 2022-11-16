cbb91c829d0f62c02870e746afe72c07acf32d4e
Nícolas <nicolasbrga01@gmail.com>
2022-11-16 17:47:49 -0300

    não sei o que tá acontecendo

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { createProductElement } from './helpers/shopFunctions';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import product from './tests/mocks/product';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
