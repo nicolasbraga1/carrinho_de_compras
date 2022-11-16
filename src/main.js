4b3f2992b6797810b66a17f9dce41dc5e11b1fab
Nícolas <nicolasbrga01@gmail.com>
2022-11-16 17:12:04 -0300

    lint me quebrou

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import {createProductElement} from './helpers/shopFunctions';
-import {fetchProductsList} from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const productsList = fetchProductsList('computador');
 const addProductsToPage = () => {
-createProductElement(productsList);
-const products = document.querySelector('.products');
-products.appendChild(createProductElement);
-}
+  createProductElement(productsList);
+  const products = document.querySelector('.products');
+  products.appendChild(createProductElement);
+};
 addProductsToPage();
