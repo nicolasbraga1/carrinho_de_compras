9c7f8943b8dc25b3aeb6ba2b7e1d757f4093ae4f
Nícolas <nicolasbrga01@gmail.com>
2022-11-16 17:26:35 -0300

    requisito 3 feito (tá fluindo)

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import { fetchProductsList } from './helpers/fetchFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-const productsList = fetchProductsList('computador');
-const addProductsToPage = () => {
-  createProductElement(productsList);
-  const products = document.querySelector('.products');
-  products.appendChild(createProductElement);
-};
-addProductsToPage();
+window.onload = async () => {
+  const productsList = await fetchProductsList('computador');
+  productsList.forEach((product) => {
+    const products = document.querySelector('.products');
+    products.appendChild(createProductElement(product));
+  })
+}
