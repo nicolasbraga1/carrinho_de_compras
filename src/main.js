fe6fe4f4afdaacb71803b4bc76a627d4074c8500
Nícolas <nicolasbrga01@gmail.com>
2022-11-18 17:46:01 -0300

    Requisito 9 feito(mais um dia pasando horas pra resolver em 5 ou 6 linhas)

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { createProductElement } from './helpers/shopFunctions';
-import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
+import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
+import { getSavedCartIDs } from './helpers/cartFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const cart = document.querySelector('.cart__products');
 const products = document.querySelector('.products');
+
 window.onload = async () => {
   products.innerHTML = '<p class = "loading">Carregando...</p>';
+  let getSaved = getSavedCartIDs();
   try {
+    // Criando a lista
     const productsList = await fetchProductsList('computador');
     products.innerHTML = '';
-    productsList.forEach((product) => {
-      products.appendChild(createProductElement(product));
+    productsList.forEach((item) => {
+      products.appendChild(createProductElement(item));
+    });
+    // Mantendo o carrinho
+    getSaved = await Promise.all(getSaved.map((id) => fetchProduct(id)));
+    getSaved.forEach((items) => {
+      const item = createCartProductElement(items);
+      cart.appendChild(item);
     });
   } catch (error) {
     products.innerHTML = '';
